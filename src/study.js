b6b7c80c276764c397600bd50c3190d6e0ca339f
nietiene <niyomugaboetiene53@gmail.com>
2025-04-21 10:10:02 -0700

    my react js lesson

--- src/study.js
+++ src/study.js
@@ -1,20 +1,39 @@
 
 const MyFirstApp = () => {
    
-    const data = {
-        name: 'Etiene',
-        email: 'eti@gmail.com',
-        password: 'pass3'
-};
+//     const data = {
+//         name: 'Etiene',
+//         email: 'eti@gmail.com',
+//         password: 'pass3'
+// };
+
+const data = [
+    { id: 1, name: 'Etiene', email: 'eti@gmail.com', password: 'pass3'},
+    { id: 2, name: 'Jonas', email: 'jonas@gmail.com', password: 'jona3'},
+    { id: 3, name: 'Eric', email: 'eric@gmail.com', password: 'pazzo'},
+    { id: 4, name: 'Pazzo', email: 'pazzo@gmail.com', password: 'pass3'},
+];
+
 
     return (
 
+        // <div>
+        //   <h1>Welcome: {data.name}</h1>
+        //   <p>Email: {data.email}</p>
+        //   <p>Password: {data.password}</p>
+        // </div>
+       
         <div>
-          <h1>Welcome: {data.name}</h1>
-          <p>Email: {data.email}</p>
-          <p>Password: {data.password}</p>
+          {data.map((user) => {
+           return(
+                  <div key={user.id}>
+                  <p><strong>Name:</strong> {user.name}</p> 
+                  <p><strong>Email:</strong> {user.email}</p> 
+                  <p><strong>Password:</strong> {user.password}</p> 
+                </div>     
+            )})}
         </div>
-    )
+    );
 } 
 
 export default MyFirstApp;
