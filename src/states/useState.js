e56550acf2316b0d5c6e4f8053128c9b8e591d94
nietiene <niyomugaboetiene53@gmail.com>
2025-04-23 11:26:09 -0700

    my react js lesson

--- src/states/useState.js
+++ src/states/useState.js
@@ -15,15 +15,31 @@ const Program = () => {
     }
 
     return (
-        <div className="bg-white shadow-lg w-70 h-auto flex flex-col items-center p-6 m-4 rounded-lg ">
-            <p className="text-xl font-semibold mb-4 font-mono"> {count}</p>
-            <div className="flex space-x-5 m-5"> 
-               <button onClick={Increment} className=" font-bold bg-blue-500 p-2 rounded-md hover:bg-blue-700 text-white">Increment</button>
-               <button onClick={reset} className="bg-red-300 p-2 rounded-md hover:bg-red-700 text-white font-bold">Reset</button>
-               <button onClick={decrement} className="bg-yellow-300 p-2 rounded-md hover:bg-yellow-700 text-white font-bold">Decrement</button>
-            </div>
+        // <div className="bg-white shadow-lg w-[40%] h-auto flex flex-col items-center p-6 m-4 rounded-lg ">
+        //     <p className="text-xl font-semibold mb-4 font-mono"> {count}</p>
+        //     <div className="flex space-x-5 m-5"> 
+        //        <button onClick={Increment} className=" font-bold bg-blue-500 p-2 rounded-md hover:bg-blue-700 text-white">Increment</button>
+        //        <button onClick={reset} className="bg-red-300 p-2 rounded-md hover:bg-red-700 text-white font-bold">Reset</button>
+        //        <button onClick={decrement} className="bg-yellow-300 p-2 rounded-md hover:bg-yellow-700 text-white font-bold">Decrement</button>
+        //     </div>
             
+        // </div>
+        <div className="h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white shadow-lg w-[40%] h-auto flex flex-col items-center p-6 m-4 rounded-lg">
+          <p className="text-xl font-semibold mb-4 font-mono">{count}</p>
+          <div className="flex space-x-5 m-5"> 
+            <button onClick={Increment} className="font-bold bg-blue-500 p-2 rounded-md hover:bg-blue-700 text-white">
+              Increment
+            </button>
+            <button onClick={reset} className="font-bold bg-red-300 p-2 rounded-md hover:bg-red-700 text-white">
+              Reset
+            </button>
+            <button onClick={decrement} className="font-bold bg-yellow-300 p-2 rounded-md hover:bg-yellow-700 text-white">
+              Decrement
+            </button>
+          </div>
         </div>
+      </div>
     )
 }
 
